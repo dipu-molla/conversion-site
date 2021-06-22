a1bc3225cb1ccfc105be27b9bd7596271e2a1d55
dipu-molla <84987172+dipu-molla@users.noreply.github.com>
2021-06-22 10:13:02 +0600

    conversion

---
+++ page1.php
@@ -0,0 +1,24 @@
+<html>
+<head><title> First Page </title>  </head>
+<body>
+<fieldset>
+ <font color="red"><h2><b>Page 1 [Home]</b></h1></font>
+ <a  target="_blank" href="Page1.php">1.Home</a>
+ <a  target="_blank" href="Page2.php">2.Conversion Rate</a>
+ <a  target="_blank" href="Page3.php">3.History</a>
+ 
+
+  <font color="red"><h2>Conversion Site</h1></font>
+ <select>
+ <option value="0" style ="display:none" selected> feet to inch</option>
+ <option>3</option>
+ <option>4</option>
+ <option>5</option>
+ </select><br><br>
+ 
+ value:<input type="text"> <br><br>
+ result:<input type="text">
+ 
+ </fieldset>
+ </body>
+ </html>
